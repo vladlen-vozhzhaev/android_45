503a9492f0cf3badff1632a7d3d30039307c72bd
Vlalden <vladlen@vozhzhaev.ru>
2025-01-15 21:42:28 +0300

    first commit

---
+++ app/src/main/java/com/example/chat_45/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.chat_45;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Socket socket = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button sendBtn = findViewById(R.id.sendBtn);
+        EditText editText = findViewById(R.id.userMsg);
+        TextView messageBox = findViewById(R.id.messageBox);
+        RecyclerView messageList = findViewById(R.id.messageList);
+        ArrayList<String> testText = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            testText.add("TEST_TEXT_"+i);
+        }
+
+        MessageAdapter messageAdapter = new MessageAdapter(testText);
+        messageList.setAdapter(messageAdapter);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket("192.168.1.9", 9123);
+                    DataInputStream is = new DataInputStream(socket.getInputStream());
+                    while (true){
+                        String response = is.readUTF();
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                messageBox.append("\n"+response);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+
+        sendBtn.setOnClickListener((e)->{
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        String msg = String.valueOf(editText.getText());
+                        out.writeUTF(msg);
+                    }catch (IOException exception){
+                        exception.printStackTrace();
+                    }
+                }
+            });
+            thread1.start();
+        });
+    }
+
+
+    public class MassageHolder extends RecyclerView.ViewHolder{
+        TextView testText;
+        public MassageHolder(LayoutInflater inflater, ViewGroup itemView) {
+            super(inflater.inflate(R.layout.message, itemView, false));
+            testText = itemView.findViewById(R.id.testText);
+        }
+        public void bind(String text){
+            testText.setText(text);
+        }
+    }
+    public class MessageAdapter extends RecyclerView.Adapter<MassageHolder>{
+        ArrayList<String> testText;
+        public MessageAdapter(ArrayList<String> testText) {
+            this.testText = testText;
+        }
+
+        @Override
+        public MassageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            return new MassageHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(MassageHolder holder, int position) {
+            holder.bind(testText.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return testText.size();
+        }
+    }
+
+
+}
